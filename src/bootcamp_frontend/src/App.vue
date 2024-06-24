1e8d60a5473bac56f7e7d8f03aa6ccc2d5e9d323
Xenonus <90400636+Xenonus@users.noreply.github.com>
2024-06-24 19:07:22 +0200

    Added an increment button

--- src/bootcamp_frontend/src/App.vue
+++ src/bootcamp_frontend/src/App.vue
@@ -4,5 +4,24 @@
     <h1>
       Gaming
     </h1>
+    <br>
+    Num: {{ i }}
+    </br>
+    <button @click="increment">Increment</button>
+
   </main>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      i: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.i++
+    }
+  },
+}
+</script>
